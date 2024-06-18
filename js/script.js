6a0272c8926778aec8281fc549163c3d62a0c6d1
cccaelum <ccc.trad@gmail.com>
2024-06-18 12:49:00 +0200

    terminado

--- js/script.js
+++ js/script.js
@@ -1,37 +1,61 @@
-const characterList = document.getElementById('character-list');
-const nextPageButton = document.getElementById('next-page');
-const prevPageButton = document.getElementById('prev-page');
-
 /*fetch('https://rickandmortyapi.com/api/character/1')
   .then(response => response.json())
   .then(data => console.log(data.species));*/
 
-
-  window.addEventListener('DOMContentLoaded', async () => {
-    const data = await loadCharacters()
-    renderCharacters(data.results)
-  })
-
-  async function loadCharacters() {
-    const response = await fetch('https://rickandmortyapi.com/api/character/?page=1')
-    return await response.json()
+  window.addEventListener('DOMContentLoaded', () => {
+    let currentPage = 1;
+    let totalPages = 0;
+    updateCharacters();
+  
+  
+  function loadCharacters(page) {
+    return fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('La solicitud no fue exitosa');
+        }
+        return response.json();
+      })
+      .then(data => {
+        totalPages = data.info.pages;
+        return data.results;
+      });
   }
-
-  const createCharacterItem = characters => characters.map(character =>        
-    `<li>
-    <img src="${character.image}" alt="${character.name}">
-    <br>
-    Name: ${character.name} <br> Species: ${character.species}
-  </li>
-`).join('');
-
-
+  
   function renderCharacters(characters) {
-    const itemsString = createCharacterItem(characters)
-    characterList.innerHTML = itemsString
+    const characterList = document.getElementById('character-list');
+    characterList.innerHTML = characters.map(character => `
+      <li>
+        <img src="${character.image}" alt="${character.name}"> <br>
+        <p><strong>Name:</strong> ${character.name}</p> <p><strong>Species:</strong> ${character.species}</p>
+      </li>`).join('');
   }
 
-
-  nextPageButton.addEventListener('click', () => {})
-
-  prevPageButton.addEventListener('click', () => {})
+  function updateCharacters() {
+    loadCharacters(currentPage)
+      .then(characters => {
+        renderCharacters(characters);
+      })
+      .catch(error => {
+        console.error('Error al cargar personajes:', error);
+      });
+  }
+  
+  const nextPageButton = document.getElementById('next-page');
+  const prevPageButton = document.getElementById('prev-page');
+  
+  nextPageButton.addEventListener('click', () => {
+    if (currentPage < totalPages) {
+      currentPage++;
+      updateCharacters();
+    }
+  });
+  
+  prevPageButton.addEventListener('click', () => {
+    if (currentPage > 1) {
+      currentPage--;
+      updateCharacters();
+    }
+  });
+});
+  
